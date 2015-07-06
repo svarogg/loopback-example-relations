4af8985780db5c4629b09e4a79251ae5add27b08
Mike Zak <mike@perfectedtech.com>
2015-07-06 16:47:58 +0300

    My stuff

--- common/models/order.js
+++ common/models/order.js
@@ -1,3 +1,19 @@
 module.exports = function(Order) {
+	Order.test = function(cb){
+		console.log(1);
+		Order.find({"include":["customer"]}, function(err, customers){
+			console.log(2);
+			console.log(customers);
+			cb(null, customers);
+		});
+	}
 
+    Order.remoteMethod(
+	    'test',
+	    {
+	        returns: {arg: 'orders', type: 'Array', root: true},
+	        description: "blabla.",
+	        http: {path: '/test', verb: 'GET'}
+	    }
+    );
 };
